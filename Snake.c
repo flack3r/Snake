9052419a1e276341e3e085264ec428cca1b14cc9
flack3r <kyj9206@gmail.com>
2015-11-14 02:38:05 +0900

    first map

--- Snake.c
+++ Snake.c
@@ -1,11 +1,46 @@
 #include <stdio.h>
+#include <curses.h>
+#include "map.h"
+
+
+void StageOneDraw()
+{
+	int i=0;
+	int j=0;
+
+	//화면 클리어
+	clear();
+	//맵 출력
+	for(i=0;i<STAGE1ROW;i++)
+	{
+		for(j=0;j<STAGE1COL;j++)
+		{
+			// 맵이 벽일 경우
+			if(FirstMap[i][j] == WALL)
+			{
+				//printf("%d,%d \n",i,j);
+				move(i,j);
+				addstr("-");
+			}
+			// 빈 공간일 경우
+			else if(FirstMap[i][j] == EMPTY)
+			{
+				continue;
+			}
+		}
+	}
+
+	refresh();
+
+}
 
 int main()
 {
-	printf("hi\n");
-	printf("wow index register\n");
-	printf("collision2");
-	printf("merge collision \n");
-	printf("hey");
+	initscr();
+	StageOneDraw();
+
+	getch();
+	endwin();
+
 	return 0;
 }
